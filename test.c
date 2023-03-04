c740ab7b9d3ebdd63b5e8b591956d3073063c736
zasabri <zasabri@gmail.com>
2023-03-04 15:34:00 +0100

    @5tirner

---
+++ test.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/*!CODING_GAME*/
+
+int	check_errors(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if ((str[i] != '1' && str[i] != '0') || str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	binary_to_decimal(char *str)
+{
+	int	l = strlen(str) - 1;
+	int base = 1;
+	int number = 0;
+	while (l >= 0)
+	{
+		if (str[l] == '1')
+			number += base;
+		base *= 2;
+		l--;
+	}
+	return (number);
+}
+
+int main(int ac, char **av)
+{
+	if (ac >= 2)
+	{
+		int i = 1;
+		while (av[i])
+		{
+			if (check_errors(av[i]))
+			{
+				printf("Somthing Wrong with this argement: `%s'\n", av[i]);
+				if (av[i + 1])
+					i++;
+				else
+					break;
+			}
+			int k = binary_to_decimal(av[i]);
+			printf("%d\n", k);
+			i++;
+		}
+	}
+}
